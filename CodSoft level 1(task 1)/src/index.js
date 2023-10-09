9e3579ced7a9fc0eecc3837c07a023a619ef9f66
Tharuniyaa Jayakanth <104960840+Tharuniyaa0810@users.noreply.github.com>
2023-10-09 22:21:14 +0530

    Add files via upload

---
+++ CodSoft level 1(task 1)/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Calculator from './Calculator';
+import PetLandingPage from './LandingPage';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <PetLandingPage />
+  </React.StrictMode>
+);
+
